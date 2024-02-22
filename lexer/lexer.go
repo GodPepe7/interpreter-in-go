fbd27b3e2d4f28dc13ebd67bc21ef2088448dc91
Alexander Mai <83711446+GodPepe7@users.noreply.github.com>
2024-02-22 21:26:35 +0100

    add lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,106 @@
+package lexer
+
+import "interpreter_in_go/token"
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	lex := &Lexer{input: input}
+	lex.readChar()
+	return lex
+}
+
+// Exercise: support Unicode (and Emojis)?
+// Hint: use rune instead of byte for ch
+func (lex *Lexer) readChar() {
+	if lex.readPosition >= len(lex.input) {
+		// 0 is in ASCII code for "NUL" character
+		lex.ch = 0
+	} else {
+		lex.ch = lex.input[lex.readPosition]
+	}
+	lex.position = lex.readPosition
+	lex.readPosition += 1
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (lex *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	lex.skipWhitespace()
+
+	switch lex.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, lex.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, lex.ch)
+	case '(':
+		tok = newToken(token.LPAREN, lex.ch)
+	case ')':
+		tok = newToken(token.RPAREN, lex.ch)
+	case ',':
+		tok = newToken(token.COMMA, lex.ch)
+	case '+':
+		tok = newToken(token.PLUS, lex.ch)
+	case '{':
+		tok = newToken(token.LBRACE, lex.ch)
+	case '}':
+		tok = newToken(token.RBRACE, lex.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(lex.ch) {
+			tok.Literal = lex.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(lex.ch) {
+			tok.Literal = lex.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, lex.ch)
+		}
+	}
+
+	lex.readChar()
+	return tok
+}
